24be70c77e4fae4f6e218ac59d068f829e7119cf
phuminhquangpham <phuminhquang.pham@student.uts.edu.au>
2023-10-13 18:27:35 +1100

    place the scanner at the end effector

--- ur3scanning.m
+++ ur3scanning.m
@@ -2,6 +2,8 @@ r = UR3();
 q = zeros(1,6);
 rfkine = r.model.fkine(q);
 
+
 hold on
 
-s = PlaceObject('barcodescannercolored.ply', [0,0,0]);
+s = PlaceObject('barcodescannercolored.ply', [X,Y,Z]);
+
