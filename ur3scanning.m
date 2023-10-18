1de1c04338b300d3bc516f6f680820552b5f8518
phuminhquangpham <phuminhquang.pham@student.uts.edu.au>
2023-10-18 17:36:52 +1100

    attach holder to UR3

--- ur3scanning.m
+++ ur3scanning.m
@@ -1,11 +1,11 @@
 r = UR3();
 q = zeros(1,6);
 rfkine = r.model.fkine(q);
-rfkineT = rfkine.T
+rfkineT = rfkine.T;
 X = rfkineT(1,4);
 Y = rfkineT(2,4);
 Z = rfkineT(3,4);
 hold on
 
-s = PlaceObject('barcodescannercolored.ply', [X,Y,Z-0.05]);
+s = PlaceObject('barcodescanner2.ply', [X,Y,Z]);
 
