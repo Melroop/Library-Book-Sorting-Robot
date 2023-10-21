7921ab971bcc2c808a47c3352bbaa2ce1b959aae
phuminhquangpham <phuminhquang.pham@student.uts.edu.au>
2023-10-21 15:07:51 +1100

    fix orienation of scanner

--- ur3scanning.m
+++ ur3scanning.m
@@ -5,15 +5,19 @@ q0 = zeros(1, 6);
 
 rfkine = r.model.fkine(q0);
 rfkineT = rfkine.T;
-X = rfkineT(1, 4);
-Y = rfkineT(2, 4);
-Z = rfkineT(3, 4);
+X0 = rfkineT(1, 4);
+Y0 = rfkineT(2, 4);
+Z0 = rfkineT(3, 4);
 
 % Create the scanner object at the initial end effector position
-scanner = PlaceObject('barcodescanner3.ply', [X, Y, Z]);
-verts = [get(scanner, 'Vertices'), ones(size(get(scanner, 'Vertices'), 1), 1)] * trotz(pi/2);
+scanner = PlaceObject('barcodescanner4.ply', [X0, Y0, Z0]);
+verts = [get(scanner, 'Vertices'), ones(size(get(scanner, 'Vertices'), 1), 1)];
+% verts = [get(scanner, 'Vertices'), ones(size(get(scanner, 'Vertices'), 1), 1)];
 verts(:, 1) = verts(:, 1);
 set(scanner, 'Vertices', verts(:, 1:3))
+scannerinitOrientation = eye(3);
+scannerinitPosition = [X0 Y0 Z0];
+scannerinitPose = [scannerinitOrientation, scannerinitPosition'; 0,0,0,1];
 
 book1initOrientation = eye(3);
 book1initPosition = [-0.6, -0.3, 0];
@@ -47,12 +51,12 @@ for i = 1:size(qtraj1, 1)
     % Update the end effector pose
     rposition = r.model.getpos();
     endEffectorPose = r.model.fkine(rposition).T;
-    X1 = endEffectorPose(1, 4);
-    Y1 = endEffectorPose(2, 4);
-    Z1 = endEffectorPose(3, 4);
 
     % Update the scanner's position
-    scanner = PlaceObject('barcodescanner3.ply', [X1, Y1, Z1]);
+    scanner = PlaceObject('barcodescanner4.ply', [X0, Y0, Z0]);
+    scannerTransform = endEffectorPose * inv(scannerinitPose);  % Calculate the transformation
+    newVerts = (verts(:, 1:3) * scannerTransform(1:3, 1:3)') + scannerTransform(1:3, 4)';
+    set(scanner, 'Vertices', newVerts);
 end
 
 for i = 1:size(qtraj2, 1)
@@ -69,11 +73,11 @@ for i = 1:size(qtraj2, 1)
     % Update the end effector pose
     rposition = r.model.getpos();
     endEffectorPose = r.model.fkine(rposition).T;
-    X2 = endEffectorPose(1, 4);
-    Y2 = endEffectorPose(2, 4);
-    Z2 = endEffectorPose(3, 4);
 
     % Update the scanner's position
-    scanner = PlaceObject('barcodescanner3.ply', [X2, Y2, Z2]);
+    scanner = PlaceObject('barcodescanner4.ply', [X0, Y0, Z0]);
+    scannerTransform = endEffectorPose * inv(scannerinitPose);   % Calculate the transformation
+    newVerts = (verts(:, 1:3) * scannerTransform(1:3, 1:3)') + scannerTransform(1:3, 4)';
+    set(scanner, 'Vertices', newVerts);
 end
 end
