014d79af87a8cf879d6eceb27984cf554ea1f5cc
phuminhquangpham <phuminhquang.pham@student.uts.edu.au>
2023-10-21 12:39:58 +1100

    delete previous frames of scanner, need orientation update

--- ur3scanning.m
+++ ur3scanning.m
@@ -1,4 +1,5 @@
 function ur3scanning()
+hold on
 r = UR3();
 q0 = zeros(1, 6);
 
@@ -7,14 +8,12 @@ rfkineT = rfkine.T;
 X = rfkineT(1, 4);
 Y = rfkineT(2, 4);
 Z = rfkineT(3, 4);
-hold on
 
-% scannerTranslation = [X Y Z];
-% scannerTransform = [eye(3), scannerTranslation'; 0,0,0,1];
+% Create the scanner object at the initial end effector position
 scanner = PlaceObject('barcodescanner3.ply', [X, Y, Z]);
-% scannerVerts = [get(scanner, 'Vertices'), ones(size(get(scanner, 'Vertices'), 1), 1)];
-% scannernewVerts = (scannerVerts(:, 1:3) * scannerTransform(1:3, 1:3)') + scannerTransform(1:3, 4)';
-% set(scanner, 'Vertices', scannerVerts);
+verts = [get(scanner, 'Vertices'), ones(size(get(scanner, 'Vertices'), 1), 1)] * trotz(pi/2);
+verts(:, 1) = verts(:, 1);
+set(scanner, 'Vertices', verts(:, 1:3))
 
 book1initOrientation = eye(3);
 book1initPosition = [-0.6, -0.3, 0];
@@ -40,14 +39,19 @@ for i = 1:size(qtraj1, 1)
     drawnow();
     pause(0.01);  % Add a small delay to slow down the animation
 
-    % Delete the previous scanner object
+    % Delete the previous scanner object (if it exists)
+    if exist('scanner', 'var') && ishandle(scanner)
+        delete(scanner);
+    end
 
     % Update the end effector pose
     rposition = r.model.getpos();
     endEffectorPose = r.model.fkine(rposition).T;
-    X1 = endEffectorPose(1,4);
-    Y1 = endEffectorPose(2,4);
-    Z1 = endEffectorPose(3,4);
+    X1 = endEffectorPose(1, 4);
+    Y1 = endEffectorPose(2, 4);
+    Z1 = endEffectorPose(3, 4);
+
+    % Update the scanner's position
     scanner = PlaceObject('barcodescanner3.ply', [X1, Y1, Z1]);
 end
 
@@ -57,12 +61,19 @@ for i = 1:size(qtraj2, 1)
     drawnow();
     pause(0.01);  % Add a small delay to slow down the animation
 
+    % Delete the previous scanner object (if it exists)
+    if exist('scanner', 'var') && ishandle(scanner)
+        delete(scanner);
+    end
+
     % Update the end effector pose
     rposition = r.model.getpos();
     endEffectorPose = r.model.fkine(rposition).T;
-    X2 = endEffectorPose(1,4);
-    Y2 = endEffectorPose(2,4);
-    Z2 = endEffectorPose(3,4);
+    X2 = endEffectorPose(1, 4);
+    Y2 = endEffectorPose(2, 4);
+    Z2 = endEffectorPose(3, 4);
+
+    % Update the scanner's position
     scanner = PlaceObject('barcodescanner3.ply', [X2, Y2, Z2]);
 end
 end
