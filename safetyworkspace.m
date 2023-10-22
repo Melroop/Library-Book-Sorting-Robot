4ee4fdd0f06f3056ec14923b83b03138f8ca96b8
EdsterTwo <edan.anonuevo@gmail.com>
2023-10-22 20:44:26 +1100

    Remove book control in this function

--- safetyworkspace.m
+++ safetyworkspace.m
@@ -3,7 +3,7 @@ function safetyworkspace()
     clf;
     clear all; 
     
-%     axis([-2,2,-2,2,-2,2]);
+    axis([-2,2,-2,2,0,3.5]);
     hold on;
     
     xlabel ('X');
@@ -12,17 +12,6 @@ function safetyworkspace()
 
     % Carpet
     surf([-2,-2;2,2],[-2,2;-2,2],[0.0,0.0;0.0,0.0],'CData',imread('carpet.jpg'),'FaceColor','texturemap');
-
-%     % Book
-%     book1 = PlaceObject('Bluebook.ply', [0.65,0.65,0.65]);
-%     book1vert = [get(book1,'Vertices'), ones(size(get(book1,'Vertices'),1),1)];
-%     % book1vert(:,1) = book1vert(:,1) * 0.5;  % scale X
-%     % book1vert(:,2) = book1vert(:,2) * 0.5;  % scale Y
-%     % book1vert(:,3) = book1vert(:,3) * 1;  % scale Z
-%     % book1vert(:,2) = book1vert(:,1) - 1;  % set X coordinate
-%     % book1vert(:,2) = book1vert(:,2) - 1;  % set Y coordinate
-%     % book1vert(:,3) = book1vert(:,3) - 1;  % set Z coordinate
-%     set(book1,'Vertices',book1vert(:,1:3))
     
     % Trolley
     trolley = PlaceObject('Trolley.ply',[0,0,0]);
@@ -32,7 +21,7 @@ function safetyworkspace()
     % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
     set(trolley,'Vertices',trolleyvert(:,1:3))
 
-    % Bookshelf
+    % Bookshelf 1
     Bookshelf = PlaceObject('Bookshelf.ply', [0,1.2,0]); 
     Bookshelfvert = [get(Bookshelf,'Vertices'), ones(size(get(Bookshelf,'Vertices'),1),1)];
     % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
