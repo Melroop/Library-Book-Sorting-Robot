970a8a6d18660ab2d3b09eb8a906b60621e70f6a
EdsterTwo <edan.anonuevo@gmail.com>
2023-10-25 18:17:17 +1100

    Updated model locations

--- safetyworkspace.m
+++ safetyworkspace.m
@@ -3,7 +3,7 @@ function safetyworkspace()
     clf;
     clear all; 
     
-    axis([-5,5,-10,5,0,4]);
+    axis([-5,10,-5,5,0,4]);
     hold on;
     
     xlabel ('X');
@@ -11,40 +11,27 @@ function safetyworkspace()
     zlabel ('Z');
 
     % Carpet
-    surf([-4.5,-4.5;4,4],[-9,3.5;-9,3.5],[0.0,0.0;0.0,0.0],'CData',imread('carpet.jpg'),'FaceColor','texturemap');
+    surf([-3.5,9;-3.5,9],[-4.5,-4.5;4,4],[0.0,0.0;0.0,0.0],'CData',imread('carpet.jpg'),'FaceColor','texturemap');
 
-    % Library
+    % Library Walls, Bookshelves, EStop
     library = PlaceObject('Librarybase.ply',[0,0,0]);
     libraryvert = [get(library,'Vertices'), ones(size(get(library,'Vertices'),1),1)];
-    % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
-    % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
-    % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
     set(library,'Vertices',libraryvert(:,1:3))
-
-    % % Trolley
-    % trolley = PlaceObject('Trolley.ply',[0,0,0]);
-    % trolleyvert = [get(trolley,'Vertices'), ones(size(get(trolley,'Vertices'),1),1)];
-    % % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
-    % % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
-    % % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
-    % set(trolley,'Vertices',trolleyvert(:,1:3))
-    
     
     % Barrier
     barrier = PlaceObject('Barriersingle.ply', [0,0,0]); 
     barriervert = [get(barrier,'Vertices'), ones(size(get(barrier,'Vertices'),1),1)];
-    % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
-    % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
-    % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
     set(barrier,'Vertices',barriervert(:,1:3))
 
     % Table Set
     table = PlaceObject('Tables.ply', [0,0,0]); 
     tablevert = [get(table,'Vertices'), ones(size(get(table,'Vertices'),1),1)];
-    % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
-    % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
-    % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
     set(table,'Vertices',tablevert(:,1:3))
+
+    % Trolley
+    trolley = PlaceObject('Trolley.ply',[-0.15,0.0,0]);
+    trolleyvert = [get(trolley,'Vertices'), ones(size(get(trolley,'Vertices'),1),1)];
+    set(trolley,'Vertices',trolleyvert(:,1:3))
 
     % hold off;
 end
