74b4fa7840d7e91dd564b622fd180f5045751117
Melroop <melroop02@gmail.com>
2023-10-25 14:52:15 +1100

    Now the environment should work and i deleted the extra stuff we shouldn't need

--- safetyworkspace.m
+++ safetyworkspace.m
@@ -14,7 +14,7 @@ function safetyworkspace()
     % surf([-2,-2;2,2],[-2,2;-2,2],[0.0,0.0;0.0,0.0],'CData',imread('carpet.jpg'),'FaceColor','texturemap');
 
     % Library
-    library = PlaceObject('Library.ply',[0,0,0]);
+    library = PlaceObject('Librarybase.ply',[0,0,0]);
     libraryvert = [get(library,'Vertices'), ones(size(get(library,'Vertices'),1),1)];
     % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
     % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
@@ -28,22 +28,23 @@ function safetyworkspace()
     % % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
     % % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
     % set(trolley,'Vertices',trolleyvert(:,1:3))
-    % 
-    % % Bookshelf 1
-    % Bookshelf = PlaceObject('Bookshelf.ply', [0,1.2,0]); 
-    % Bookshelfvert = [get(Bookshelf,'Vertices'), ones(size(get(Bookshelf,'Vertices'),1),1)];
-    % % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
-    % % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
-    % % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
-    % set(Bookshelf,'Vertices',Bookshelfvert(:,1:3))
-    % 
-    % % Barrier (rotate with trotz)
-    % barrier = PlaceObject('Barrier.ply', [0,-1.5,0]); 
-    % barriervert = [get(barrier,'Vertices'), ones(size(get(barrier,'Vertices'),1),1)];
-    % % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
-    % % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
-    % % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
-    % set(barrier,'Vertices',barriervert(:,1:3))
+    
+    
+    % Barrier
+    barrier = PlaceObject('Barriersingle.ply', [0,0,0]); 
+    barriervert = [get(barrier,'Vertices'), ones(size(get(barrier,'Vertices'),1),1)];
+    % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
+    % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
+    % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
+    set(barrier,'Vertices',barriervert(:,1:3))
+
+    % Table Set
+    table = PlaceObject('Tables.ply', [0,0,0]); 
+    tablevert = [get(table,'Vertices'), ones(size(get(table,'Vertices'),1),1)];
+    % trolleyvert(:,1) = trolleyvert(:,1) * 0.1;  % scale X
+    % trolleyvert(:,2) = trolleyvert(:,2) * 0.1;  % scale Y
+    % trolleyvert(:,3) = trolleyvert(:,3) * 0.1;  % scale Z
+    set(table,'Vertices',tablevert(:,1:3))
 
     % hold off;
 end
